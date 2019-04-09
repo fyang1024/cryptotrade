0a02311476baeadaab63cba36bb84f3a4c56f9a3
fyang1024 <fyang1024@gmail.com>
2019-04-09 18:04:43 +1000

    Fix price to 2 decimal points

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { start } from './redux/actions'
 import './App.css'
 
 const mapStateToProps = state => ({
+  count: state.count,
   trades: state.trades,
   stats: state.stats
 })
+
 
 class App extends Component {
 
@@ -20,21 +22,20 @@ class App extends Component {
   }
 
   render() {
-    const { trades, stats } = this.props
-    console.log('stats', stats)
+    const { count, stats } = this.props
     return (
       <div className="App">
         <button onClick={this.dispatchStart}>Start</button>
-        <p>{trades && trades.length}</p>
-        { stats &&
-          <table align='center' width='80%'>
+        <p>Trades: {count}</p>
+        { stats && Object.keys(stats).length &&
+          <table align='center' width='68%'>
             <tbody>
             <tr>
               <th>Price</th>
               <th>Buy Volume</th>
               <th>Sell Volume</th>
               <th>Total Volume</th>
-              <th>Time</th>
+              <th>Trades</th>
             </tr>
             {
               Object.keys(stats).map((key, index) =>
@@ -43,7 +44,7 @@ class App extends Component {
                   <td>{stats[key].buy}</td>
                   <td>{stats[key].sell}</td>
                   <td>{stats[key].total}</td>
-                  <td>{stats[key].time}</td>
+                  <td>{stats[key].trades}</td>
                 </tr>
               )
             }
